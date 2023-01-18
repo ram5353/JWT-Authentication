39be95438adcb9b6d6bcc39e3236f6f075e805f4
ram5353 <ramkiran5353@gmail.com>
2023-01-18 01:34:14 -0500

    generateOTP, verifyOTP, resetPassword api's

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -2,6 +2,7 @@ import UserModel from "../model/User.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import ENV from "../config.js";
+import otpGenerator from "otp-generator";
 
 /** middleware to verify user */
 export async function verifyUser(req, res, next) {
@@ -138,24 +139,23 @@ export async function login(req, res) {
 export async function getUser(req, res) {
   const { username } = req.params;
 
-    try {
-        
-        if(!username) return res.status(501).send({ error: "Invalid Username"});
+  try {
+    if (!username) return res.status(501).send({ error: "Invalid Username" });
 
-        UserModel.findOne({ username }, function(err, user){
-            if(err) return res.status(500).send({ err });
-            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+    UserModel.findOne({ username }, function (err, user) {
+      if (err) return res.status(500).send({ err });
+      if (!user)
+        return res.status(501).send({ error: "Couldn't Find the User" });
 
-            /** remove password from user */
-            // mongoose return unnecessary data with object so convert it into json
-            const { password, ...rest } = Object.assign({}, user.toJSON());
+      /** remove password from user */
+      // mongoose return unnecessary data with object so convert it into json
+      const { password, ...rest } = Object.assign({}, user.toJSON());
 
-            return res.status(201).send(rest);
-        })
-
-    } catch (error) {
-        return res.status(404).send({ error : "Cannot Find User Data"});
-    }
+      return res.status(201).send(rest);
+    });
+  } catch (error) {
+    return res.status(404).send({ error: "Cannot Find User Data" });
+  }
 }
 
 /** PUT: http://localhost:8080/api/updateuser 
@@ -168,26 +168,95 @@ body: {
     profile : ''
 }
 */
-export async function updateUser(req,res){
+export async function updateUser(req, res) {
   try {
-      
-      const {userId} = req.user;
+    const { userId } = req.user;
 
-      if(userId){
-          const body = req.body;
+    if (userId) {
+      const body = req.body;
 
-          // update the data
-          UserModel.updateOne({ _id : userId }, body, function(err, data){
-              if(err) throw err;
+      // update the data
+      UserModel.updateOne({ _id: userId }, body, function (err, data) {
+        if (err) throw err;
 
-              return res.status(201).send({ msg : "Record Updated...!"});
-          })
-
-      }else{
-          return res.status(401).send({ error : "User Not Found...!"});
-      }
-
+        return res.status(201).send({ msg: "Record Updated...!" });
+      });
+    } else {
+      return res.status(401).send({ error: "User Not Found...!" });
+    }
   } catch (error) {
-      return res.status(401).send({ error });
+    return res.status(401).send({ error });
+  }
+}
+
+/** GET: http://localhost:8080/api/generateOTP */
+export async function generateOTP(req, res) {
+  req.app.locals.OTP = await otpGenerator.generate(6, {
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
+  res.status(201).send({ code: req.app.locals.OTP });
+}
+
+/** GET: http://localhost:8080/api/verifyOTP */
+export async function verifyOTP(req, res) {
+  const { code } = req.query;
+  if (parseInt(req.app.locals.OTP) === parseInt(code)) {
+    req.app.locals.OTP = null; // reset the OTP value
+    req.app.locals.resetSession = true; // start session for reset password
+    return res.status(201).send({ msg: "Verify Successsfully!" });
+  }
+  return res.status(400).send({ error: "Invalid OTP" });
+}
+
+// successfully redirect user when OTP is valid
+/** GET: http://localhost:8080/api/createResetSession */
+export async function createResetSession(req, res) {
+  if (req.app.locals.resetSession) {
+    return res.status(201).send({ flag: req.app.locals.resetSession });
+  }
+  return res.status(440).send({ error: "Session expired!" });
+}
+
+// update the password when we have valid session
+/** PUT: http://localhost:8080/api/resetPassword */
+export async function resetPassword(req, res) {
+  try {
+    if (!req.app.locals.resetSession)
+      return res.status(440).send({ error: "Session expired!" });
+
+    const { username, password } = req.body;
+
+    try {
+      UserModel.findOne({ username })
+        .then((user) => {
+          bcrypt
+            .hash(password, 10)
+            .then((hashedPassword) => {
+              UserModel.updateOne(
+                { username: user.username },
+                { password: hashedPassword },
+                function (err, data) {
+                  if (err) throw err;
+                  req.app.locals.resetSession = false; // reset session
+                  return res.status(201).send({ msg: "Record Updated...!" });
+                }
+              );
+            })
+            .catch((e) => {
+              return res.status(500).send({
+                error: "Enable to hashed password",
+              });
+            });
+        })
+        .catch((error) => {
+          return res.status(404).send({ error: "Username not Found" });
+        });
+    } catch (error) {
+      return res.status(500).send({ error });
+    }
+  } catch (error) {
+    return res.status(401).send({ error });
   }
 }
--- server/router/route.js
+++ server/router/route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 import * as controller from '../controllers/appController.js';
 import {verifyUser} from '../controllers/appController.js';
-import Auth from "../middleware/auth.js";
+import Auth, { localVariables } from "../middleware/auth.js";
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
@@ -11,8 +11,12 @@ router.route('/login').post(verifyUser, controller.login);
 
 /** Get Methods */
 router.route('/user/:username').get(controller.getUser) // user with username
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP) // generate random OTP
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP) // verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession) // reset all the variables
 
 /** PUT Methods */
 router.route('/updateuser').put(Auth, controller.updateUser);
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword); // use to reset password
 
 export default router;
