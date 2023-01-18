0d76e043e41ca6674080b502c5ea9bfcca05fb89
ram5353 <ramkiran5353@gmail.com>
2023-01-17 21:23:14 -0500

    added register component with validations

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -23,6 +23,15 @@ export async function resetPasswordValidation(values){
     return errors;
 }
 
+/** validate register form */
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
 /** validate username */
 function usernameVerify(error = {}, values){
     if(!values.username){
@@ -49,4 +58,17 @@ function passwordVerify(errors = {}, values){
     }
 
     return errors;
+}
+
+/** validate email */
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong Email...!")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
+    }
+
+    return error;
 }
