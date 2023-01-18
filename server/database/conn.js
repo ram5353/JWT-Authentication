cca6f30f79f0a01e217b76d7ad9c3115ad27a737
ram5353 <ramkiran5353@gmail.com>
2023-01-18 06:43:49 -0500

    heroku deployment chanes

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
-import { MongoMemoryServer } from "mongodb-memory-server";
-
 async function connect(){
-
-    const mongod = await MongoMemoryServer.create();
-    const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true)
     // const db = await mongoose.connect(getUri);
