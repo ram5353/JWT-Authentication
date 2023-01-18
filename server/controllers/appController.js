5b1940bec270dade9aaef221d1132cb0d87e5aa0
ram5353 <ramkiran5353@gmail.com>
2023-01-18 00:56:13 -0500

    getUser and update user api

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,21 +1,19 @@
 import UserModel from "../model/User.model.js";
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken';
-import ENV from '../config.js';
+import jwt from "jsonwebtoken";
+import ENV from "../config.js";
 
 /** middleware to verify user */
-export async function verifyUser(req, res, next){
+export async function verifyUser(req, res, next) {
   try {
-      
-      const { username } = req.method == "GET" ? req.query : req.body;
+    const { username } = req.method == "GET" ? req.query : req.body;
 
-      // check the user existance
-      let exist = await UserModel.findOne({ username });
-      if(!exist) return res.status(404).send({ error : "Can't find User!"});
-      next();
-
+    // check the user existance
+    let exist = await UserModel.findOne({ username });
+    if (!exist) return res.status(404).send({ error: "Can't find User!" });
+    next();
   } catch (error) {
-      return res.status(404).send({ error: "Authentication Error"});
+    return res.status(404).send({ error: "Authentication Error" });
   }
 }
 
@@ -133,5 +131,63 @@ export async function login(req, res) {
       });
   } catch (error) {
     return res.status(500).send({ error });
+  }
+}
+
+/** GET: http://localhost:8080/api/user/example123 */
+export async function getUser(req, res) {
+  const { username } = req.params;
+
+    try {
+        
+        if(!username) return res.status(501).send({ error: "Invalid Username"});
+
+        UserModel.findOne({ username }, function(err, user){
+            if(err) return res.status(500).send({ err });
+            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+
+            /** remove password from user */
+            // mongoose return unnecessary data with object so convert it into json
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+
+            return res.status(201).send(rest);
+        })
+
+    } catch (error) {
+        return res.status(404).send({ error : "Cannot Find User Data"});
+    }
+}
+
+/** PUT: http://localhost:8080/api/updateuser 
+ * @param: {
+  "header" : "<token>"
+}
+body: {
+    firstName: '',
+    address : '',
+    profile : ''
+}
+*/
+export async function updateUser(req,res){
+  try {
+      
+      const userId = req.query.id;
+
+      if(userId){
+          const body = req.body;
+
+          // update the data
+          UserModel.updateOne({ _id : userId }, body, function(err, data){
+              if(err) throw err;
+
+              return res.status(201).send({ msg : "Record Updated...!"});
+          })
+
+      }else{
+          return res.status(401).send({ error : "User Not Found...!"});
+      }
+
+  } catch (error) {
+      return res.status(401).send({ error });
   }
 }
--- server/router/route.js
+++ server/router/route.js
@@ -6,9 +6,12 @@ import {verifyUser} from '../controllers/appController.js';
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
-
-/** Get Methods */
 router.route('/login').post(verifyUser, controller.login);
 
+/** Get Methods */
+router.route('/user/:username').get(controller.getUser) // user with username
+
+/** PUT Methods */
+router.route('/updateuser').put(controller.updateUser);
 
 export default router;
